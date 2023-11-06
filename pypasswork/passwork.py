9c364e4cb055e2c3b1ffa441c6d9c879d6e0eed1
Alexander <asand3r@ya.ru>
2023-11-06 14:15:34 +0300

    Version 0.0.2 features (#1)
    Changes:
    * add passwords api to search and get passwords
    * remove _search_password and _get_password method from core PasswordAPI
      class
    *
    
    Co-authored-by: Alexander Khatsayuk <aahatsayuk@sbercloud.ru>

--- pypasswork/passwork.py
+++ pypasswork/passwork.py
@@ -1,13 +1,13 @@
-import base64
 import logging
 from socket import gaierror
-from typing import Optional
 
 from requests import Request, Session
 from requests.exceptions import ConnectionError, ConnectTimeout, JSONDecodeError
 
 from pypasswork.exceptions import PassworkInteractionError
 from pypasswork.response import PassworkResponse
+from pypasswork.operations.passwords import Passwords
+
 
 logger = logging.getLogger(__name__)
 
@@ -18,41 +18,38 @@ class PassworkAPI:
     Parameters:
         url: URl to connect to API (e.g: https://passwork.me)
         key: API key to authorize with
-        vault_name: name of Passwork API to work with
-
     Returns:
         PassworkResponse instance
     Raises:
         PassworkInteractionError
     """
 
-    def __init__(self, url: str, key: str, vault_name: Optional[str] = None):
+    def __init__(self, url: str, key: str):
         self._url = url
         self._key = key
         self._session = Session()
-        self._vault_name = vault_name
         self.__token = None
-        self._vault_id = None
 
-        self.__token = self._login()
-        self._vault_id = self._get_vault_id()
-        
+        self._login()
+        self.passwords = Passwords(self)
+
     def __repr__(self) -> str:
-        return f'PassworkAPI(url={self._url}, vault={self._vault_name})'
+        return f'PassworkAPI(url={self._url})'
 
     @property
     def _headers(self) -> dict[str, str]:
+        headers = {'Content-Type': 'application/json'}
         if self.__token:
-            return {'Passwork-Auth': self.__token, 'Content-Type': 'application/json'}
-        else:
-            return {}
+            headers['Passwork-Auth'] = self.__token
+        
+        return headers
 
-    def _make_request(self,
-                      method: str,
-                      endpoint: str,
-                      parameters: dict[str, str] | None = None,
-                      timeout: int = 5
-                      ) -> PassworkResponse:
+    def make_request(self,
+                     method: str,
+                     endpoint: str,
+                     parameters: dict[str, str] | None = None,
+                     timeout: int = 5
+                     ) -> PassworkResponse:
         """
         Make base request to PassworkAPI and return result
 
@@ -89,22 +86,24 @@ class PassworkAPI:
 
         return PassworkResponse(status=data['status'], data=data['data'])
 
-    def _login(self) -> str:
+    def _login(self) -> None:
         """
         Login to Passwork API with given key and get auth token string.
+        Fetched token writes to self.__token attribute and uses in
+        all PasswordAPI requests as 'Passwork-Auth' header value.
 
         Returns:
-            Passwork API token
+            None
         """
 
         logger.debug('In _login')
 
         try:
-            resp = self._make_request('POST', endpoint=f'/auth/login/{self._key}')
+            resp = self.make_request('POST', endpoint=f'/auth/login/{self._key}')
         except PassworkInteractionError as e:
             raise PassworkInteractionError(f'Passwork login failed: {e}')
 
-        return resp.data['token']
+        self.__token = resp.data['token']
 
     def _logout(self) -> None:
         """
@@ -112,74 +111,4 @@ class PassworkAPI:
 
         logger.debug('In _logout')
 
-        self._make_request('POST', endpoint='/auth/logout')
-
-    def _get_vault_id(self) -> Optional[str]:
-        """
-        Search vaults in Passwork and return ID of given Vault name
-
-        Returns:
-            VaultId as a string or None if vault name not provided
-        """
-
-        logger.debug('In _get_vault_id')
-
-        if not self._vault_name:
-            return None
-
-        resp = self._make_request('GET', endpoint='/vaults/list', timeout=10)
-
-        # FIXME: check KeyError
-        for vault in resp.data:
-            if vault['name'] == self._vault_name:
-                return vault['id']
-        else:
-            raise PassworkInteractionError(f'Cannot find vault_id for given vault name: "{self._vault_name}"')
-
-    def search_password(self, password_name: str) -> Optional[str]:
-        """
-        Implement search password in Passwork with given entry name.
-
-        Args:
-            password_name: Passwork password name to search
-
-        Returns:
-            Passwork as a string
-        """
-
-        logger.debug('In search_password')
-
-        search_params = {'query': password_name}
-        if self._vault_id:
-            search_params['vaultId'] = self._vault_id
-
-        resp = self._make_request('POST', endpoint='/passwords/search', parameters=search_params, timeout=10)
-
-        for password in resp.data:
-            if password['name'] == password_name:
-                return self._get_password(password['id'])
-        else:
-            logger.debug(f'Cannot find passwork with name: "{password_name}"')
-            return None
-
-    def _get_password(self, password_id: str) -> Optional[str]:
-        """
-        Retrieve password information by ID
-
-        Args:
-            password_id: passwork id to get from Passwork
-        Returns:
-            Password as a string
-        Raises:
-            PassworkInteractionError
-        """
-
-        logger.debug('In _get_password')
-
-        try:
-            resp = self._make_request('GET', endpoint=f'/passwords/{password_id}')
-        except PassworkInteractionError as e:
-            logger.error(f'Cannot get password wih ID "{password_id}": {e}')
-        else:
-            password = base64.b64decode(resp.data["cryptedPassword"]).decode('utf-8')
-            return password
+        self.make_request('POST', endpoint='/auth/logout')
--- pypasswork/response.py
+++ pypasswork/response.py
@@ -1,22 +1,28 @@
-from ast import Pass
+from enum import Enum
 from collections.abc import Mapping
 from typing import Any, Literal, TypeAlias
 
 
 PassworkData: TypeAlias = str | Mapping[str, Any] | list[Mapping[str, Any]]
- 
+
+
+class PassworkStatus(Enum):
+    SUCCESS = 0
+    FAILED = 1
+
+
 class PassworkResponse:
     def __init__(self, status: Literal['success', 'failed'], data: PassworkData):
         self._status = status
         self._data = data
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(status={self._status}, data={self.data})'
+        return f'{self.__class__.__name__}(status={self.status.value}, data={self.data})'
 
     @property
-    def status(self) -> int:
-        return 1 if self._status == 'success' else 0
+    def status(self) -> PassworkStatus:
+        return PassworkStatus[self._status.upper()]
 
     @property
-    def data(self) -> str | Mapping[str, Any] | list[Mapping[str, Any]]:
+    def data(self) -> PassworkData:
         return self._data
